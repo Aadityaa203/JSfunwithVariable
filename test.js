82c7add7f9195c3b8ed604c4b8cb4dbf8b91f58d
chudaa203 <chudaa203@gmail.com>
2023-07-19 17:55:36 -0400

    JS fun with Variables

---
+++ test.js
@@ -0,0 +1,44 @@
+// 1. Declare ten different variables and assign them the value of a number.
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 5;
+var f = 6;
+var g = 7;
+var h = 8;
+var i = 9;
+var j = 10;
+//2. Two times, console.log the addition of two of the variables you declared.
+var a = 1;
+var b = 2;
+console.log(a + b);
+console.log(a + b);
+
+//3. Two times, console.log the subtraction of two of the variables you declared.
+var c = 3;
+var d = 4;
+console.log(c + d);
+console.log(c + d);
+
+//4. Two times, console.log the multiplication of two of the variables you declared.
+var e = 5;
+var f = 6;
+console.log(e * f);
+console.log(e * f);
+
+//5. Two times, console.log the division of two of the variables you declared.
+var g = 7;
+var h = 8;
+console.log(g / h);
+console.log(g / h);
+
+//6. Two times, increments one of the variables you declared.
+var i = 9;
+console.log(i++);
+console.log(i++);
+
+//7. Two times, decrement one of the variables you have declared.
+var j = 10;
+console.log(j--);
+console.log(j--);
